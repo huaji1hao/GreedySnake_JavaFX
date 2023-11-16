c9221c4a7651ee8d4835711c9512e850801d740f
scyjz14 <huaji1hao@gmail.com>
2023-11-17 06:56:32 +0800

    add a skin for the snake in the candy background

--- src/main/java/com/megasnake/game/view/GameView.java
+++ src/main/java/com/megasnake/game/view/GameView.java
@@ -34,7 +34,7 @@ public class GameView {
             } else if(difficulty == 1){
                 gc.drawImage(new Image(getClass().getResource("/background/candy_background.png").toURI().toString()), 0, 0, WIDTH, HEIGHT);
             } else if(difficulty == 2){
-                gc.drawImage(new Image(getClass().getResource("/background/lava_background.png").toURI().toString()), 0, 0, WIDTH, HEIGHT);
+                gc.drawImage(new Image(getClass().getResource("/background/lava_background.jpg").toURI().toString()), 0, 0, WIDTH, HEIGHT);
             } else if(difficulty == 3){
                 scrollingBackground.draw(gc);
             }
@@ -50,8 +50,14 @@ public class GameView {
         gc.drawImage(food.getFoodImage(), food.getX() * SQUARE_SIZE, food.getY() * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
     }
 
-    public void drawSnake(GraphicsContext gc, Snake mySnake) {
+    public void drawSnake(GraphicsContext gc, Snake mySnake, int difficulty) {
         try {
+            String snakeFolderPath;
+
+            if(difficulty == 1) snakeFolderPath = "/snake/candy-snake/";
+            else if(difficulty == 2) snakeFolderPath = "/snake/plain-snake/";
+            else if(difficulty == 4) snakeFolderPath = "/snake/plain-snake/";
+            else snakeFolderPath = "/snake/plain-snake/";
 
             // Calculate the movement frame which determines how much the snake has progressed towards the next cell
             double moveFrame = mySnake.getMoveFrame();
@@ -67,7 +73,7 @@ public class GameView {
                 double partY = currentPart.getY() + (prevPart.getY() - currentPart.getY()) * moveFrame;
 
                 // Draw the current body part with a slight reduction in size for visual fineness
-                gc.drawImage(new Image(getClass().getResource("/snake/plain-snake/snake-body.png").toURI().toString()), partX * SQUARE_SIZE, partY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
+                gc.drawImage(new Image(getClass().getResource(snakeFolderPath + "snake-body.png").toURI().toString()), partX * SQUARE_SIZE, partY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
             }
 
             // Get the first part of the snake for drawing
@@ -77,38 +83,32 @@ public class GameView {
             double headX = headPart.getX();
             double headY = headPart.getY();
             int currentDirection = DirectionHandler.getCurrentDirection();
-            if (currentDirection == DirectionHandler.RIGHT) {
-                headX += moveFrame;
-            } else if (currentDirection == DirectionHandler.LEFT) {
-                headX -= moveFrame;
-            } else if (currentDirection == DirectionHandler.UP) {
-                headY -= moveFrame;
-            } else if (currentDirection == DirectionHandler.DOWN) {
-                headY += moveFrame;
+            switch (currentDirection) {
+                case DirectionHandler.RIGHT -> headX += moveFrame;
+                case DirectionHandler.LEFT -> headX -= moveFrame;
+                case DirectionHandler.UP -> headY -= moveFrame;
+                case DirectionHandler.DOWN -> headY += moveFrame;
             }
 
             // Draw the snake head with the correct orientation based on the current direction of movement
             switch (currentDirection) {
-                case DirectionHandler.RIGHT:
+                case DirectionHandler.RIGHT ->
                     // Draw the head facing right
-                    gc.drawImage(new Image(getClass().getResource("/snake/plain-snake/snake-head-right.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
-                    break;
-                case DirectionHandler.LEFT:
+                        gc.drawImage(new Image(getClass().getResource(snakeFolderPath + "snake-head-right.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
+                case DirectionHandler.LEFT ->
                     // Draw the head facing left
-                    gc.drawImage(new Image(getClass().getResource("/snake/plain-snake/snake-head-left.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
-                    break;
-                case DirectionHandler.UP:
+                        gc.drawImage(new Image(getClass().getResource(snakeFolderPath + "snake-head-left.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
+                case DirectionHandler.UP ->
                     // Draw the head facing up
-                    gc.drawImage(new Image(getClass().getResource("/snake/plain-snake/snake-head-up.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
-                    break;
-                case DirectionHandler.DOWN:
+                        gc.drawImage(new Image(getClass().getResource(snakeFolderPath + "snake-head-up.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
+                case DirectionHandler.DOWN ->
                     // Draw the head facing down
-                    gc.drawImage(new Image(getClass().getResource("/snake/plain-snake/snake-head-down.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
-                    break;
+                        gc.drawImage(new Image(getClass().getResource(snakeFolderPath + "snake-head-down.png").toURI().toString()), headX * SQUARE_SIZE, headY * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
             }
         } catch (Exception e) {
             System.out.println("Error loading snake image: " + e.getMessage());
         }
+
     }
 
 
@@ -122,7 +122,7 @@ public class GameView {
     public void drawAll(GraphicsContext gc, Snake mySnake, Food food, int difficulty){
         drawBackground(gc, difficulty);
         drawFood(gc, food);
-        drawSnake(gc, mySnake);
+        drawSnake(gc, mySnake, difficulty);
         drawScore(gc, mySnake.getScore());
     }
 
